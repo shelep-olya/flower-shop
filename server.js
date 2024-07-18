6a66bcd75828f0c80d60764a8207b45cbaef2abe
shelep-olya <shelep.olya@gmail.com>
2024-07-18 15:45:52 +0300

    establishing views & fixing bug with footer styles

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const viewsRouter = require("./routes/viewsRouter")
+const cookieParser = require('cookie-parser');
 dotenv.config({ path: "./config.env" });
 
 const app = express();
-
+const PORT = 3000;
 const MONGO_URL = process.env.MONGODB_URI;
 
 mongoose.connect(MONGO_URL)
@@ -17,12 +19,14 @@ mongoose.connect(MONGO_URL)
   process.exit(1);
 });
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use("/", viewsRouter);
 
-app.use(bodyParser.json());
 
-
-
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
 });
